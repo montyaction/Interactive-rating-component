1a7742904c2aacbf42b2f952ce4f6d61cbe9dc13
spider-Monty <montyaction@hotmail.com>
2022-07-08 22:19:20 +0530

    Project compeleted

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log("hello there are js file connected");
+
+const ratingsForm = document.getElementById("ratings-form");
+
+const ratings = document.getElementById("ratings");
+
+const thanks = document.getElementById("thanks");
+
+ratingsForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    const userRating = document.querySelector('input[name="rating"]:checked');
+
+    if (userRating !== null) {
+
+        ratings.classList.toggle("hide-section");
+        
+        thanks.classList.toggle("hide-section");
+
+        document.getElementById("user-choice").innerHTML = userRating.ariaValueMax; 
+    }
+});
